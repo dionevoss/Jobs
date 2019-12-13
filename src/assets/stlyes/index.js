e30edbd93708564c8107eaed34c853c630b8989f
Dionatan Voss <dionevoss@gmail.com>
2019-12-13 18:07:11 -0300

    Add logo and styles

---
+++ src/assets/stlyes/index.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { 
+    Button,
+    Paper,
+
+} from '@material-ui/core'
+
+export const ButtonStyle = styled(Button)`
+    && {
+        background-color: #000
+        height: 50px
+        color: white
+        padding: 0px 55px 0px 55px
+
+        :hover {
+            background-color: #7C7C7C
+        }
+        
+        a {
+            text-decoration: none
+            color: white
+        }
+    }
+`
+export const PaperStyle = styled(Paper)`
+    && {
+        margin: 5%;
+    }
+`
+
+export const TextStyle = styled.h1`
+    color: #7C7C7C
+    font-size: 20px
+    padding-bottom: 25px
+`
+
+export const SpacerStyle = styled.div`
+    padding: 8px
+`
