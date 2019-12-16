1000a7460d066401bfd8ddc3acef82630e4828be
Dionatan Voss <dionevoss@gmail.com>
2019-12-16 16:35:18 -0300

    Add link on logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 import { HeaderStyle } from './styled'
 import logo from '../../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 
 export default () => (
     <HeaderStyle>
-        <img src={logo} alt="logo"/>
+        <Link to='/'>
+            <img src={logo} alt="logo"/>
+        </Link> 
     </HeaderStyle>
 )
