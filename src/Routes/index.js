845f96c28e993399d2e8b0ac895d53f6e3257a24
Dionatan Voss <dionevoss@gmail.com>
2019-12-16 16:37:42 -0300

    Add route CreateUser and fix profile route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,13 +5,15 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
 import Profile from '../pages/Profile'
 import UpdateUser from '../pages/UpdateUser'
+import CreateUser from '../pages/CreateUser'
 
 export default () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/profile/:id' component={Profile} />
             <Route exact path='/update' component={UpdateUser} />
+            <Route exact path='/create' component={CreateUser} />
         </Switch>
     </BrowserRouter>
 )
