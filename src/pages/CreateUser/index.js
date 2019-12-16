555e2ffae99725910a12b1d7bd7e05ccca2c3281
Dionatan Voss <dionevoss@gmail.com>
2019-12-16 16:38:42 -0300

    Create User Creation page

---
+++ src/pages/CreateUser/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+import UserCreate from '../../components/UserCreate'
+import Header from '../../components/Header'
+
+export default () => (
+    <Fragment>
+        <Header />
+        <UserCreate />
+    </Fragment>
+)
